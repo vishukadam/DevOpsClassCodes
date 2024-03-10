5d1fcf0ddb0c0091c1c6d12a2161540d968afab1
vishalkadam <vishukadam@gmail.com>
2024-03-10 21:57:24 +0530

    Update ContactForm.java

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -27,7 +27,7 @@ public class ContactForm extends FormLayout {
     Button cancel = new Button("Cancel", this::cancel);
     TextField firstName = new TextField("First name");
     TextField lastName = new TextField("Last name");
-    TextField phone = new TextField("Mobilesss");
+    TextField phone = new TextField("Mobile");
     TextField email = new TextField("Emails");
     DateField birthDate = new DateField("Birth date");
 
